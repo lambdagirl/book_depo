76a0f7ad848271c08782569e5e22c10b4f3c162a
Peizhi Zheng <pzheng16@asu.edu>
2020-05-19 00:41:24 -0700

    ensured that images match a given format.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
-
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -28,6 +29,15 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price=1
     assert product.valid?
+  end
 
+  test "product is not valid without a unique title" do 
+    product = Product.new(
+      title: products(:ruby).title,
+      description:"yyy",
+      price:1,
+      image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"],product.errors[:title]
   end
 end
