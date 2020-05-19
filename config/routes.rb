3968893b81dc883e1904a092b3d3a857d29f6aa9
Peizhi Zheng <pzheng16@asu.edu>
2020-05-19 00:57:26 -0700

    add catalog display

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get ’store/index’ # as: 'store_index' tells Rails to create store_index_path and store_index_url accessor methods
+  root 'store#index', as: 'store_index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
